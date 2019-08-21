0300bd70f70778df586f434aa2c6ac414d594764
Ghelbendorf <erez.ghelbendorf@intel.com>
2019-08-21 23:06:34 +0300

    Enablement fixes - package engine and debug comments removed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express= require('express')
 const bodyParser= require('body-parser')
 const cors = require('cors')
-const PORT = 3000
-// const api = require('./routes/api')
-// const Odata = require('./routes/Odata')
+const PORT = process.env.PORT || 5000
+const api = require('./routes/api')
+const Odata = require('./routes/Odata')
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-// app.use('/api',api)
-// app.use('/Odata',Odata)
+app.use('/api',api)
+app.use('/Odata',Odata)
 app.get('/', function(req, res){
     res.send('Hello from server')
 })
